d4196d20dc2ffb344d0d152cb748980f779e975e
saydurasulov11 <tursunovazimbek28@gmail.com>
2026-09-08 20:12:19 +0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Pixora from './Pixora.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Pixora />
+  </React.StrictMode>
+)
