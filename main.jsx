af934ccd408fe5ed909cfa19ae6026dccbb7657c
saydurasulov11 <tursunovazimbek28@gmail.com>
2026-09-08 20:15:40 +0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Pixora from './Pixora.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Pixora />
-  </React.StrictMode>
-)
